7e744bae697c508a6b0f945c55a1ffe9ff3690df
joaocm1804 <joaocarlosmendes18@gmail.com>
2026-05-18 17:07:50 -0300

    feat: add Camera collectible class with flash effect logic

--- src/collectibles/Camera.hpp
+++ src/collectibles/Camera.hpp
@@ -1,4 +1,8 @@
-#include "collectible.hpp"
+#ifndef CAMERA_HPP
+#define CAMERA_HPP
+#include "Collectible.hpp"
+#include <map>
+#include <string>
 #include <cmath>
 
 class Camera : public Base {
@@ -34,3 +38,5 @@ public:
         }
     }
 };
+
+#endif
