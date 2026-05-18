1b390e846b21184352e34297c1b844836399c84b
joaocm1804 <joaocarlosmendes18@gmail.com>
2026-05-18 17:08:26 -0300

    feat: refactor Game.cpp and create game.py for improved structure and functionality

--- src/core/Game.cpp
+++ src/core/Game.cpp
@@ -1,59 +1,56 @@
+#include <Collectible.hpp>
+#include <Banana.hpp>
+#include <Camera.hpp>
+#include <Rosa.hpp>
+#include <Player.hpp>
+#include <CollisionSystem.hpp>
+#include <Spawner.hpp>
 #include <iostream>
-#include <string>
-#include <ctime>
 #include <vector>
 #include <map>
 using namespace std;
 
-// # inicializando pygame + criando tela + inicializando variáveis globais + aceleração gradual do cenário
-// altura_tela = 700
-// largura_tela = 900
-// dimensoes_tela = (largura_tela, altura_tela)
-// pygame.init()    
-// pygame.display.set_caption('Gisele Bundchen VS As Forças Do Mal')
-// tela = pygame.display.set_mode(dimensoes_tela)
 
-// DAR UMA OLHADA DEPOIS
+//inicializando pygame + criando tela + inicializando variáveis globais + aceleração gradual do cenário
+int altura_tela = 700;
+int largura_tela = 900;
+//dimensoes_tela = (largura_tela, altura_tela)
+//pygame.init()    
+//pygame.display.set_caption('Gisele Bundchen VS As Forças Do Mal')
+// tela = pygame.display.set_mode(dimensoes_tela)
 // relogio = pygame.time.Clock()
 // ACELERAR = pygame.USEREVENT + 1
 // pygame.time.set_timer(ACELERAR, 10000)
 
-// # importando classes
-#include "Player.hpp"
-#include "Collectible.hpp"
-#include "CollisionSystem.hpp"
-#include "Spawner.hpp"
-// from cenarios.tela_inicial import TelaInicial
-// from cenarios.tela_final import TelaFinal
-// from cenarios.desfile import Desfile
-// from personagens.gisele import Gisele
-// from coletaveis.base import Base
-// from coletaveis.banana import Banana
-// from coletaveis.camera import Camera
-// from coletaveis.rosa import Rosa
 
-//temporario
-struct Coletavel{
-    int x, y;
-    string nome;
-};
-bool run = true;
+// # instanciando objetos para acessar a classe
+// base_engine = Base()
+// gisele = Gisele()
+// banana = Banana()
+// camera = Camera()
+// tela_inicial = TelaInicial(tela)
 
-int main(){
- 
-    run = true;
+Player gisele;
+Banana banana;
+Base base_engine;
+Camera camera;
+
+
+
+
+bool jogo(){
+
+    //global run 
+    bool run = true;;
     bool finalizando = false;
     bool perdeu = false;
-    // som_vitoria_played = false
-    // som_derrota_played = false
-
-    //dependo dos outros arquivos
-    gisele.player_y = 500; // ou a posição inicial dela
-    gisele.player_x = 100;
-    bool gisele.esta_pulando = false;
+    bool som_vitoria_played = false;
+    bool som_derrota_played = false;
+    gisele.setPosition(100, 500); //= 500 # ou a posição inicial dela
+    //gisele.player_x = 100
+    //gisele.esta_pulando = false;
     bool transicao_final = false;
 
-    //PROVAVELMENTE NAO VOU USAR
     // # configurando cenário
     // tela_inicial.rodar()
     // bg_image, bg_width, tiles, scroll = Desfile.iniciar_passarela(dimensoes_tela, largura_tela)
@@ -64,107 +61,143 @@ int main(){
     int meta_metros = 700;
     int vel_coletavel = 7;
 
-    //dicionarios com os contadores
+    // # dicionários com os contadores
     // contadores = {
     //     "banana": 0,   
     //     "camera": 0,   
     //     "rosa": 0    
     // }
 
-    map<string, int> contadores = {{"banana", 0}, {"camera", 0}, {"rosa", 0}};
+    map<string, int> contadores = {{"Banana", 0}, {"Rosa", 0}, {"Moeda", 0}};
 
-    vector<Coletavel> coletaveis;
 
-    for (int i = 0; i <3;i++){
-        vector<int> alturas_ocupadas;
-        vector<int> xs_ocupados;
-        for (Coletavel c: coletaveis){
-            alturas_ocupadas.push_back(c.y);
-            xs_ocupados.push_back(c.x);
+    vector<ColetavelData> coletaveis;
+
+    for (int i = 0; i <3; i++){
+        vector<int> alturas_ocupadas;// = [c["rect"].y for c in coletaveis]
+        vector<int> xs_ocupados;// = [c["rect"].x for c in coletaveis]
+
+        for (auto &c: coletaveis){
+            alturas_ocupadas.push_back(c.rect.y);
+            xs_ocupados.push_back(c.rect.x);
         }
-        coletaveis.push_back(gerar_coletavel(alturas_ocupadas, xs_ocupados, largura_tela)); //ajustar depois
+
+        //######################################//######################################
+        //######################################//######################################
+        //######################################//######################################
+        //sprite_coletavel = random.choice(base_engine.sprites_coletaveis)
+        coletaveis.push_back(base_engine.gerar_coletavel(alturas_ocupadas, xs_ocupados, largura_tela)); //MODIFICAR
+        //     base_engine.gerar_coletavel(
+        //         sprite_coletavel,
+        //         alturas_ocupadas,
+        //         xs_ocupados,
+        //         largura_tela
+        //     )
+        // )
+        //######################################//######################################
+        //######################################//######################################
+        //######################################//######################################
+
     }
 
-    // loop principal
-    while (run == true){
-        //variaveis locais
-        delta_time = relogio.tick(30) / 1000.0 // tempo decorrido em segundos
-        meta_metros = 500
-        tela.fill((0,0,0))
+    //# loop principal
+    while (run == true;){
+        //# variaveis locais
+        float delta_time = 1.0/30;//relogio.tick(30) / 1000.0 # tempo decorrido em segundos
+        int meta_metros = 500;
+        //tela.fill((0,0,0))
 
-        for event in pygame.event.get(): 
-            if event.type == QUIT: # fechar a janela do jogo
-                pygame.quit()
-                exit()
 
-            if event.type == KEYDOWN: # identificar o w ou a seta do pulo ou espaço
-                if event.key == K_w or event.key == K_UP or event.key == K_SPACE:
-                    gisele.pular()
+//VERIFICAR VERIFICAR VERIFICAR VERIFICAR VERIFICAR VERIFICAR VERIFICAR VERIFICAR VERIFICAR VERIFICAR VERIFICAR
+        // for event in pygame.event.get(): 
+        //     if event.type == QUIT: # fechar a janela do jogo
+        //         pygame.quit()
+        //         exit()
 
-            if event.type == ACELERAR and vel_coletavel < 20:
-                vel_coletavel += 1
+        //     if event.type == KEYDOWN: # identificar o w ou a seta do pulo ou espaço
+        //         if event.key == K_w or event.key == K_UP or event.key == K_SPACE:
+        //             gisele.pular()
 
-        # chamando as funções de gisele
-        gisele.atualizar_fisica()
-        gisele.atualizar_animacao(delta_time)
+        //     if event.type == ACELERAR and vel_coletavel < 20:
+        //         vel_coletavel += 1
+
+        //# chamando as funções de gisele
+        gisele.update_fisica(delta_time);
+        //gisele.atualizar_animacao(delta_time);
         
-        # incrementando a distância percorrida
-        distancia_pixels += 7
-        distancia_metros = distancia_pixels // pixels_por_metro
+        //# incrementando a distância percorrida
+        distancia_pixels += 7;
+        int distancia_metros = distancia_pixels / pixels_por_metro;
 
-        if not finalizando:
-            for i in range(tiles):
-                tela.blit(bg_image, (i * bg_width + scroll, 0))
-            scroll -= (7 + vel_coletavel)
-            if abs(scroll) > bg_width:
-                scroll = 0
-        if meta_metros <= distancia_metros-50: # checando finalizando de novo 
-            finalizando = True
-        if finalizando:
-            # estado de finalização: o cenário para de rodar em loop
-            # e desenhamos a passarela de chegada deslizando uma única vez
-            tela.blit(LINHA_CHEGADA, (0, 0))
+        if (meta_metros <= distancia_metros - 50) {
+            finalizando = true;;
+        }
+
+        if (finalizando == true;){
+            // for i in range(tiles):
+            //     tela.blit(bg_image, (i * bg_width + scroll, 0))
+            // scroll -= (7 + vel_coletavel)
+            // if abs(scroll) > bg_width:
+            //     scroll = 0
+            gisele.setX(gisele.get_X() + 5);
+            if (gisele.get_X() >= largura_tela){
+                 run = false; //# fim do jogo / tela de vitória
+                 transicao_final = true;;
+            }
+        }
             
-            gisele.player_x += 5
+        // if finalizando:
+        //     # estado de finalização: o cenário para de rodar em loop
+        //     # e desenhamos a passarela de chegada deslizando uma única vez
+        //     tela.blit(LINHA_CHEGADA, (0, 0))
+            
+        //     gisele.player_x += 5
 
-            if gisele.player_x >= largura_tela:
-                 run = false # fim do jogo / tela de vitória
-                 transicao_final = True
+        //     if gisele.player_x >= largura_tela:
+        //          run = false; # fim do jogo / tela de vitória
+        //          transicao_final = true;
 
 
-        if abs(scroll) > bg_width:
-            scroll = 0
+        // if abs(scroll) > bg_width:
+        //     scroll = 0
 
         # movimentação dos coletáveis
-        if not finalizando:
-            for coletavel in coletaveis:
-                coletavel["rect"].x -= vel_coletavel
+        if (finalizando == false){
+            for (auto& coletavel : coletaveis){
+                coletavel.rect.x -= vel_coletavel;
 
-                if coletavel["rect"].right < 0:
-                    alturas_ocupadas = [c["rect"].y for c in coletaveis if c != coletavel]
-                    xs_ocupados = [c["rect"].x for c in coletaveis if c != coletavel]
-                    coletavel.update(
-                        base_engine.gerar_coletavel(
-                            random.choice(base_engine.sprites_coletaveis),
-                            alturas_ocupadas,
-                            xs_ocupados,
-                            largura_tela
-                        )
-                    )
+                if (coletavel.rect.x + coletavel.rect.w < 0){
+                    vector<int> alturas_ocupadas; //= [c["rect"].y for c in coletaveis if c != coletavel]
+                    vector<float> xs_ocupados; //= [c["rect"].x for c in coletaveis if c != coletavel]
+                    
+                    for (auto &c : coletaveis){
+                        if (&c != &coletavel){
+                            alturas_ocupadas.push_back(c.rect.y);
+                            xs_ocupados.push_back(c.rect.x);
+                        }
+                    }
+                    coletavel = base_engine.gerar_coletavel(alturas_ocupadas, xs_ocupados, largura_tela); //MODIFICAR
+                //se usasse push_back, adiconaria um novo coletavel quando um coletavel saisse da tela
+                }
+            }
+        }
+            Rect gisele_rect = gisele.get_rect(); //adicionar
 
-            gisele_rect = gisele.get_rect()
-
-        //colisões
-        for coletavel in coletaveis:
-            if gisele_rect.colliderect(coletavel["rect"]):
+        //FALTAM AS COLISOES
+        //# colisões
+        for (auto &coletavel :coletaveis){
+            if (gisele_rect.colliderect(coletavel["rect"]):
                 if coletavel["coletavel"] == base_engine.sprites_coletaveis[2]:
                     Rosa.efeito_rosa(contadores)
+                    som_rosa.play()
                 else:
                     if coletavel["coletavel"] == base_engine.sprites_coletaveis[0]:
                         Banana.efeito_banana(contadores)
+                        som_banana.play()
                     elif coletavel["coletavel"] == base_engine.sprites_coletaveis[1]:
                         Camera.efeito_camera(contadores)
                         camera.iniciar_flash(largura_tela, altura_tela)
+                        som_camera.play()
 
                 alturas_ocupadas = [c["rect"].y for c in coletaveis if c != coletavel]
                 xs_ocupados = [c["rect"].x for c in coletaveis if c != coletavel]
@@ -176,11 +209,11 @@ int main(){
                         largura_tela
                     )
                 )
+            }
 
-        //OLHAR DEPOIS
-        //mostrando os contadores na tela
-        for coletavel in coletaveis:
-            tela.blit(coletavel["coletavel"], (coletavel["rect"]))
+        // # mostrando os contadores na tela
+        // for coletavel in coletaveis:
+        //     tela.blit(coletavel["coletavel"], (coletavel["rect"]))
 
         // # mostrando gisele na tela
         // gisele.desenhar(tela)
@@ -189,47 +222,53 @@ int main(){
         // fundo_x = 20
         // fundo_y = 8
 
-        tela.blit(HUD_BG, (fundo_x, fundo_y))
+        // tela.blit(HUD_BG, (fundo_x, fundo_y))
 
-        margin_x = fundo_x + 20
+        // margin_x = fundo_x + 20
 
-        texto_distancia = FONTE_HUD.render(f"{distancia_metros} m", True, (0, 0, 0))
-        tela.blit(texto_distancia, (margin_x, fundo_y + 55))
+        // texto_distancia = FONTE_HUD.render(f"{distancia_metros} m", true;, (0, 0, 0))
+        // tela.blit(texto_distancia, (margin_x, fundo_y + 55))
 
-        icones = ["banana", "camera", "rosa"]
-        for i, sprite in enumerate(base_engine.sprites_coletaveis):
-            tela.blit(sprite, (margin_x, 25))
-            nome_item = icones[i]
-            texto = FONTE_HUD.render(str(contadores[nome_item]), True, (0, 0, 0))
-            tela.blit(texto, (margin_x + 45, 30))
-            margin_x += 65
+        // icones = ["banana", "camera", "rosa"]
+        // for i, sprite in enumerate(base_engine.sprites_coletaveis):
+        //     tela.blit(sprite, (margin_x, 25))
+        //     nome_item = icones[i]
+        //     texto = FONTE_HUD.render(str(contadores[nome_item]), true;, (0, 0, 0))
+        //     tela.blit(texto, (margin_x + 45, 30))
+        //     margin_x += 65
 
-        //flash do paparazzi
-        camera.desenhar_flash(tela, dimensoes_tela)
+        // # flash do paparazzi
+        // camera.desenhar_flash(tela, dimensoes_tela)
 
-        # fim do jogo
-        if contadores["banana"] >= 3 or contadores["camera"] >= 3:
-            run = false
-            perdeu = True
-            # tela de game over
-        elif distancia_metros >= meta_metros-50:
-            finalizando = True
-        pygame.display.update()
+        //# fim do jogo
+        if (contadores["banana"] >= 3 || contadores["camera"] >= 3){
+            run = false;
+            perdeu = true;
+            //# tela de game over
+        }else if (distancia_metros >= meta_metros-50){
+            finalizando = true;
+        }
+        //pygame.display.update()
 
-        if perdeu:
-            # Chame o método da tela final que exibe derrota
-            tela_final = TelaFinal(tela, contadores, distancia_metros, meta_metros)
-            return tela_final.run() // retornar True para reiniciar ou false para sair
-                
-        if finalizando:
-        if transicao_final:
-            tela_final = TelaFinal(tela, contadores, distancia_metros, meta_metros)
-            return tela_final.run()
+        if (perdeu == true){
+            return false;
+        }
+        
+        if (transicao_final){
+            return false;
+        }
 
-# chamando função principal
-if __name__ == "__main__":
-    while True: # Loop de reinicialização do jogo
-        jogar_novamente = main()
-        if not jogar_novamente:
-            break
-    pygame.quit()
+    return false;
+    }
+}
+
+int main(){
+    srand(time(nullptr));
+    while (true){
+        bool jogar_novamente = jogo();
+        if (jogar_novamente == false){
+            break;
+        }
+    }
+    return 0;
+}
